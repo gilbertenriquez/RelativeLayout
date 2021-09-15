43fe75ae038347f275612ff3f6acd18968a64114
Gilbert Enriquez <gilbertenriquez10@gmail.com>
2021-09-15 10:27:45 +0800

    Add project files.

---
+++ RelativeLayout/RelativeLayout/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RelativeLayout
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
